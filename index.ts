996f54fecc2fe4801454c3c2c25eca5d78edd8ad
Ben Kung <ben_kung@infofab.com>
2019-06-13 14:40:27 +0800

    哏

--- index.ts
+++ index.ts
@@ -1,67 +1,70 @@
 import cheerio from 'cheerio';
 import iconvlite from 'iconv-lite';
 import request from 'request';
-const rootUrl=`http://tw-dentist.com/front/bin`;
-const $=cheerio;
+var json2xls = require('json2xls');
+const fs = require('fs');
 
- const  crawlUrl=async (requestUrl:any)=>{
-   return new Promise( (resolve, reject)=>{
-    request({ url: requestUrl,encoding:null} , function(err,response, body) {
-          if (!err && response.statusCode == 200) {
-            var str = iconvlite.decode(new Buffer(body), "big5");
-             resolve(str)
-          }
-        }) 
+const rootUrl = `http://tw-dentist.com/front/bin`;
+const $ = cheerio;
+
+const crawlUrl = async (requestUrl: any) => {
+  return new Promise((resolve, reject) => {
+    request({ url: requestUrl, encoding: null }, function (err, response, body) {
+      if (!err && response.statusCode == 200) {
+        var str = iconvlite.decode(new Buffer(body), "big5");
+        resolve(str)
+      }
+    })
   });
- }
-const getNestBodyData=async (root:any)=>{
+}
+const getNestBodyData = async (root: any) => {
+  console.log(root)
+  let nestedPatientQAListData: any = [];
 
-  let nestedPatientQAListData:any=[];
-
-  return new Promise( (resolve, reject)=>{
-    root.map( async (a:any)=>{
-      const  nestWebsiteBody=await crawlUrl(a.url);
-      const nestDataList=$(nestWebsiteBody).find('.shadow-link')
+  return new Promise((resolve, reject) => {
+    root.map(async (a: any) => {
+      const nestWebsiteBody = await crawlUrl(a.url);
+      const nestDataList = $(nestWebsiteBody).find('.shadow-link')
       for (let index = 0; index < nestDataList.length; index++) {
         const element = $(nestDataList[index]);
-       // console.log(element.text())
-       // console.log(rootUrl+"/"+element.attr('href'))
-        const answer=$($(await crawlUrl(rootUrl+"/"+element.attr('href'))).find('.ptdet-text').children('p')[0]).text();
-       // console.log(`answer : ${answer}`)
+        console.log(element.text())
+        console.log(rootUrl + "/" + element.attr('href'))
+        const answer = $($(await crawlUrl(rootUrl + "/" + element.attr('href'))).find('.ptdet-text').children('p')[0]).text();
+        console.log(`answer : ${answer}`)
         nestedPatientQAListData.push({
-          title:element.text(),
-          answer:answer,
-        url:rootUrl+"/"+element.attr('href')
+          title: element.text(),
+          answer: answer,
+          url: rootUrl + "/" + element.attr('href')
         })
-      } 
+      }
     })
-    resolve(nestedPatientQAListData)
+    let xls = json2xls(nestedPatientQAListData);
+    fs.writeFileSync('data.xlsx', xls, 'binary');
+    return resolve(nestedPatientQAListData)
   });
 
 
- 
 
 }
-(async()=>{
+(async () => {
   // 
- //let bb= await  ;
- //console.log(bb)
- let websiteBody=await crawlUrl(`${rootUrl}/cglist.phtml?Category=421169`)
-const patientQAList=$(websiteBody).find('.shadow-ptname');
-let patientQAListData:any=[];
-let nestedPatientQAListData:any=[];
-for (let index = 0; index < patientQAList.length; index++) {
-  const element = $(patientQAList[index]).find('a');
-  patientQAListData.push({
-    title:element.text(),
-    url:rootUrl+"/"+element.attr('href')
-  })
-  
-}
-//console.log(patientQAListData)
-getNestBodyData(patientQAListData).then(b=>{
-  console.log(b)
-});
+  //let bb= await  ;
+  //console.log(bb)
+  console.log(`${rootUrl}/cglist.phtml?Category=421169`)
+  let websiteBody = await crawlUrl(`${rootUrl}/cglist.phtml?Category=421169`)
+  console.log(websiteBody)
+  const patientQAList = $(websiteBody).find('.shadow-ptname');
+  let patientQAListData: any = [];
+  let nestedPatientQAListData: any = [];
+  for (let index = 0; index < patientQAList.length; index++) {
+    const element = $(patientQAList[index]).find('a');
+    patientQAListData.push({
+      title: element.text(),
+      url: rootUrl + "/" + element.attr('href')
+    })
+
+  }
+  await getNestBodyData(patientQAListData);
 
 
 })()
